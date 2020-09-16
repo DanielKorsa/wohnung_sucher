217392b004a901782032712ea7fa1d4dcbff4075
GM <gesundmeister@gms-imac.localdomain>
2020-09-16 22:21:42 +0200

    added bold text

--- lambda_function.py
+++ lambda_function.py
@@ -55,7 +55,7 @@ def lambda_handler(event,context):
         for fresh_deal_url in fresh_deals_urls:
             flat_info = get_flat_full_details(fresh_deal_url)
             put_item(flat_info) # update DB
-            bot_message = ' Description:{} \n Address:{} \n Price:{} \n Area:{} \n \
+            bot_message = ' <b>Description:</b>{} \n Address:{} \n Price:{} \n Area:{} \n \
             Move in date:{} \n {}'.format(flat_info['description'], flat_info['address'], flat_info['price'], flat_info['Area'], flat_info['movinDate'],flat_info['weblink'])
             bot_sendtext(bot_message, bot_token, bot_chat_id2) # send msg to group
             
