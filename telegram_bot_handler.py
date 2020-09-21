ba9df9d0a5dd4b70ab133c725c6d8c93206855e1
GM <gesundmeister@gms-imac.localdomain>
2020-09-21 21:26:41 +0200

    about to start with sending pics

--- telegram_bot_handler.py
+++ telegram_bot_handler.py
@@ -26,3 +26,14 @@ def bot_sendpic(pic_path, bot_token, bot_chat_id):
     r = requests.post(url, files=files, data=data)
     
     return (r.status_code, r.reason, r.content)
+
+
+
+def bot_send_inline_img(pic_path, bot_message, bot_token, bot_chat_id):
+    
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chat_id + '&parse_mode=HTML&text=' + bot_message
+
+    response = requests.get(send_text)
+
+    return response.json()
+    
