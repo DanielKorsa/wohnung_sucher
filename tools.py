a1c7250bd931a2ffa192ad92df06f105c3910c17
Danil Konowalow <Danil.Konowalow@nanion.de>
2020-12-02 17:43:33 +0100

    new user agent

--- tools.py
+++ tools.py
@@ -60,6 +60,14 @@ def get_header():
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': f'{random.choice(USER_AGENTS)}',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        },
+        {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Connection': 'keep-alive',
+        'DNT': '1',
+        'User-Agent': f'{random.choice(USER_AGENTS)}',
         }
 ]
 
