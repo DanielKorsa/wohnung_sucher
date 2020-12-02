cd8701c4c9b517a4083ac23331acbe6ee2b69153
Danil Konowalow <Danil.Konowalow@nanion.de>
2020-12-02 17:53:34 +0100

    added referer

--- tools.py
+++ tools.py
@@ -68,6 +68,15 @@ def get_header():
         'Connection': 'keep-alive',
         'DNT': '1',
         'User-Agent': f'{random.choice(USER_AGENTS)}',
+        },
+        {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Connection': 'keep-alive',
+        'DNT': '1',
+        'Referer' : 'https://www.google.de',
+        'User-Agent': f'{random.choice(USER_AGENTS)}',
         }
 ]
 
